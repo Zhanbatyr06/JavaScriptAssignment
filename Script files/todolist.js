e8aae0bb6e4a16b1e19769acb48c569ed321ba4a
itsmenadir <nadirsh21@gmail.com>
2024-10-22 16:16:56 +0500

    add functionality to todolist

--- Script files/todolist.js
+++ Script files/todolist.js
@@ -11,3 +11,17 @@ function CloseMenu(){
     bgfone.classList.remove('cover')
 
 }
+let tasks = [];
+
+function addTask(){
+    const taskInput = document.getElementById('taskInput');
+    const TaskList = document.getElementById('TaskList');
+    let newTask = taskInput.value;
+    if (taskInput){
+        tasks.push(newTask);
+        let li = document.createElement('li');
+        li.textContent = newTask;
+        TaskList.appendChild(li);
+        taskInput.value = '';
+    }
+}
